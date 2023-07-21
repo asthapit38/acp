b5ca78f4a54307c5553cbd5d8ccdb431f0760203
asthapit38 <31070366+asthapit38@users.noreply.github.com>
2023-07-21 14:09:54 +0545

    added logo in about

--- app/about/components/affilations.jsx
+++ app/about/components/affilations.jsx
@@ -1,9 +1,43 @@
+import WFTOLogo from "@/public/images/logos/wfto.png";
+import WFTOAsiaLogo from "@/public/images/logos/wfto-asia.png";
+import FairTradeLogo from "@/public/images/logos/fait-trade-group-logo.jpg";
+import Image from "next/image";
+
 const AboutAffiliations = () => {
   return (
     <article className="space-y-4" id="about-affiliations">
       <p className="text-2xl font-bold leading-relaxed tracking-wider text-primary font-title">
         Affiliations
       </p>
+      <div className="relative flex items-start gap-x-5">
+        <Image
+          src={WFTOLogo}
+          width={100}
+          height={100}
+          alt="World fair trade Logo"
+          style={{
+            objectFit: "contain",
+          }}
+        />
+        <Image
+          src={WFTOAsiaLogo}
+          width={100}
+          height={100}
+          alt="World fair trade asia Logo"
+          style={{
+            objectFit: "contain",
+          }}
+        />
+        <Image
+          src={FairTradeLogo}
+          width={100}
+          height={100}
+          alt="Fair Trade Logo"
+          style={{
+            objectFit: "contain",
+          }}
+        />
+      </div>
     </article>
   );
 };
--- app/components/ui/DiscoverCard.jsx
+++ app/components/ui/DiscoverCard.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Button from "./Button";
+
 
 const DiscoverCard = ({ imageSrc, imageAlt, children }) => {
   return (
